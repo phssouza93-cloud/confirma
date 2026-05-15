840280555a62d755fdba4e6beb96d64a0d8af334
João Portela <joaoportelamadureira@gmail.com>
2026-05-15 16:34:59 -0300

    feat: deploy via Docker + Swarm + GHCR

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options aqui se necessário no futuro */
+  output: "standalone",
 };
 
 export default nextConfig;
